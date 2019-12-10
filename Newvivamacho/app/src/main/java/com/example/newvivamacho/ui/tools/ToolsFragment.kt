ae1e30d294b86af2a4aba94572c1877480c7f0e7
yata <nadanagi.dc143c@gmail.com>
2019-12-10 10:08:48 +0900

    12/10 2nd

--- Newvivamacho/app/src/main/java/com/example/newvivamacho/ui/tools/ToolsFragment.kt
+++ Newvivamacho/app/src/main/java/com/example/newvivamacho/ui/tools/ToolsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -14,18 +15,30 @@ class ToolsFragment : Fragment() {
 
     private lateinit var toolsViewModel: ToolsViewModel
 
+    private var mWebView: WebView? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        toolsViewModel =
-            ViewModelProviders.of(this).get(ToolsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_tools, container, false)
-        val textView: TextView = root.findViewById(R.id.text_tools)
-        toolsViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+
+        val root = inflater.inflate(R.layout.fragment_training_webview, container, false)
+
+        // 画面のインスタンスを取得
+        mWebView = root.findViewById(R.id.webView) as WebView
+
+        // WebSettingsオブジェクトを取得
+        val settings = mWebView!!.settings
+
+        // JavaScriptを有効にする
+        settings.javaScriptEnabled = true
+
+
+        // Activity起動時に初期URL（Google日本語サイト）を読み込む
+
+        mWebView!!.loadUrl("https://smartlog.jp/65045")
+
         return root
     }
 }
